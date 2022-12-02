1fd1e8c869ab687511082253f149ec0506a67d9b
WankioM <111064857+WankioM@users.noreply.github.com>
2022-12-02 03:51:03 +0300

    Create 102-magic_calculation.py

---
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+# 102-magic_calculation.py
+# Tracy Wankio Marwa
+
+
+def magic_calculation(a, b):
+    """Match bytecode provided by Holberton School."""
+    from magic_calculation_102 import add, sub
+
+    if a < b:
+        c = add(a, b)
+        for i in range(4, 6):
+            c = add(c, i)
+        return (c)
+
+    else:
+        return(sub(a, b))
+Footer
+© 2022 GitHub, Inc.

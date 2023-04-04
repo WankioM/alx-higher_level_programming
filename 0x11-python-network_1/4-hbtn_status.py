8ac41e31b7e6398614c7808a2c416ffc7a12a1ae
WankioM <111064857+WankioM@users.noreply.github.com>
2023-04-04 12:53:00 +0300

    Update 4-hbtn_status.py

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -2,9 +2,10 @@
 """Gets https://intranet.hbtn.io/status."""
 import requests
 
+url = 'https://alx-intranet.hbtn.io/status'
 
-if __name__ == "__main__":
-    r = requests.get("https://intranet.hbtn.io/status")
-    print("Body response:")
-    print("\t- type: {}".format(type(r.text)))
-    print("\t- content: {}".format(r.text))
+response = requests.get(url)
+
+print('Body response:')
+print('\t- type:', type(response.content))
+print('\t- content:', response.content.decode())

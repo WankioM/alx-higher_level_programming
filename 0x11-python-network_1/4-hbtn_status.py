a1c8efece9faf71a6fa17f3a6278ee30f068b0e6
WankioM <111064857+WankioM@users.noreply.github.com>
2023-04-04 13:14:16 +0300

    Update 4-hbtn_status.py

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -7,5 +7,5 @@ url = 'https://alx-intranet.hbtn.io/status'
 response = requests.get(url)
 
 print('Body response:')
-print('\t- type:', type(response.content))
+print('\t- type:', type(response.text))
 print('\t- content:', response.content)

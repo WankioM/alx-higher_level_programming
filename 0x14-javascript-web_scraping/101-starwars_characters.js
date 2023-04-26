319509b78b153048043e84ae1f5467ae87adf1d5
WankioM <111064857+WankioM@users.noreply.github.com>
2023-04-27 00:25:52 +0300

    Update 101-starwars_characters.js

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -12,8 +12,8 @@ function printCharacters (characters, index) {
   request(characters[index], function (error, response, body) {
     if (!error) {
       console.log(JSON.parse(body).name);
-      if (index + 1 < characters.length) {
-        printCharacters(characters, index + 1);
+        if (index + 1 < characters.length) {
+          printCharacters(characters, index + 1);
       }
     }
   });
